d7c210f97b179a4fc15934812fb88bc246c5646c
Hanope <heemanghan@gmail.com>
2017-11-19 20:41:32 +0900

    Spring Boot REST API GET method Test 메소드 작성

---
+++ SpringTest/src/test/java/com/example/demo/EchoIT.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.Test;
+import static com.jayway.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class EchoIT {
+
+    @Test
+    public void firstEchoTest() {
+        get("/echo/hello").then().assertThat().body("message", equalTo("hello"));
+    }
+}
